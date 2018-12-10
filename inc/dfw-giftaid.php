bf1603916a367d8fe68c77644d9ee5d93d998949
Mobinet113 <benjaminroffe@mobiusit.net>
2018-12-10 10:32:46 +0000

    Option to enable UK Giftaid

--- inc/dfw-giftaid.php
+++ inc/dfw-giftaid.php
@@ -5,18 +5,22 @@
  */
 
 // Hook in
-add_filter( 'woocommerce_checkout_fields' , 'add_giftaid_field', 20 );
+add_filter( 'woocommerce_checkout_fields' , 'add_giftaid_field', 100 );
 
 // Our hooked in function - $fields is passed via the filter!
 function add_giftaid_field( $fields ) {
-  $fields['billing']['giftaid'] = array(
-    'type'      => 'checkbox',
-    'label'     => __('Giftaid it', 'woocommerce'),
-    'placeholder'   => _x('Giftaid it', 'placeholder', 'woocommerce'),
-    'required'  => false,
-    'class'     => array('form-row-wide', 'wc-donation-giftaid'),
-    'clear'     => true
-  );
+
+  // If the UK Giftaid setting is enabled, create a new checkbox in the checkout
+  if ( hm_wcdon_get_option('enable_uk_giftaid_field') ) {
+    $fields['billing']['giftaid'] = array(
+      'type' => 'checkbox',
+      'label' => __('Giftaid it', 'woocommerce'),
+      'placeholder' => _x('Giftaid it', 'placeholder', 'woocommerce'),
+      'required' => false,
+      'class' => array('form-row-wide', 'wc-donation-giftaid'),
+      'clear' => true
+    );
+  }
 
   return $fields;
 }
